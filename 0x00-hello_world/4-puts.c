d36c5f26d4c51305d599cd32d8b88e166c5396a7
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-01 09:05:46 -0700

    Third trila: Fixing the betty documentation style error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -print the phrase "Programming is like a building a multiligual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
-puts("Programming is like building multilingual puzzle\n");
+puts("\"Programming is like building multilingual puzzle\n");
 return (0);
 }
