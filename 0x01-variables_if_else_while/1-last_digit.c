5105b487331d703c3288e90d9f628d0453624c06
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-04 12:43:16 -0700

    Second trial: Creating the l1-last_digit C script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - assign a random  number to int n
- * it executes, and print n
+ * main - print the last digit of the number
+ * stored in the variable n
  * Return - 0 if succesfully, non-zero if otherwise.
  */
 
@@ -12,20 +12,20 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("The last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-	{
-	 printf("and is greater than 5\n");   
-	}
-	else if (n % 10 == 0)
-	{
-	printf("and is 0\n");
-	}
-	else if(n % 10 < 6)
-	{
-	printf("and is less than 6 and not 0\n");
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("last digit of %d is %d ", n, n % 10);
+if (n % 10 > 5)
+{
+printf("and is greater than 5\n");   
+}
+else if (n % 10 == 0)
+{
+printf("and is 0\n");
+}
+else if (n % 10 < 6)
+{
+printf("and is less than 6 and not 0\n");
+}
+return (0);
 }
