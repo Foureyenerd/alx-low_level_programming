3a44a44db285f80db27cdfb2fa2ad31ca59809c4
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-04 13:00:48 -0700

    Fixing issues

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - prints the last digit of the random
  * number stored in the variable n
- * Return - 0 if succesfully, non-zero if otherwise.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
