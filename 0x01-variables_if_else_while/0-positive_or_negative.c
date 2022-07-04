dce7a13e58afb5f8dcc9268264abf311ca444bef
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-04 12:12:02 -0700

    Forth trial: Creating and commiting the C script that print n and assign a new value evertime code is run

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+
 /*
 * main - print and assigns a random number to int n everytime
 * it executes.
